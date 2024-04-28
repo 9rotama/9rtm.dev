0fda1b8f7661ba25d4ec0b7e304724a35a269906
9rotama <aoim99.sc@gmail.com>
2024-04-29 02:11:08 +0900

    feat: add cloud and light effect to kaomoji-kun

--- src/components/layout/kaomoji-kun.tsx
+++ src/components/layout/kaomoji-kun.tsx
@@ -1,11 +1,18 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { Center, Float, OrbitControls, Text3D } from "@react-three/drei";
+import {
+  Center,
+  Cloud,
+  Float,
+  OrbitControls,
+  Sparkles,
+  Text3D,
+} from "@react-three/drei";
 
 export default function KaomojiKun() {
   return (
     <Canvas style={{ width: "100%", height: "200px" }}>
-      <KaomojiKunMesh />
+      <KaomojiKunScene />
     </Canvas>
   );
 }
@@ -20,11 +27,11 @@ const kaomojis = [
   "(#_#)",
 ];
 
-function KaomojiKunMesh() {
+function KaomojiKunScene() {
   return (
     <>
       <Center>
-        <Float>
+        <Float floatIntensity={2} rotationIntensity={2} speed={1.5}>
           <Text3D
             font="/fonts/noto_sans_bold.json"
             height={0.1}
@@ -32,12 +39,66 @@ function KaomojiKunMesh() {
             rotation={[0, -0.5 * Math.PI, 0]}
           >
             {kaomojis[0]}
-            <meshPhongMaterial attach="material" color={0xffffff} />
+            <meshStandardMaterial
+              attach="material"
+              color={0xdd88ff}
+              metalness={0.7}
+              roughness={0.2}
+              transparent
+              opacity={1}
+            />
           </Text3D>
         </Float>
-        <OrbitControls makeDefault />
       </Center>
-      <ambientLight color={0xff99ff}></ambientLight>
+      <OrbitControls makeDefault />
+      <Sparkles scale={15} count={200} />
+      <Cloud position={[6, 2, 6]} scale={1} opacity={0.3} />
+      <Cloud
+        position={[0, 14, 5]}
+        scale={1.8}
+        opacity={0.3}
+        rotation={[0, -0.4 * Math.PI, 0]}
+      />
+      <Cloud
+        position={[-5, -12, -2]}
+        scale={2}
+        opacity={0.3}
+        rotation={[0, 0.5 * Math.PI, 0]}
+      />
+      <Cloud
+        position={[-10, 4, 13]}
+        scale={2}
+        opacity={0.3}
+        rotation={[0, 0.5 * Math.PI, 0]}
+      />
+      <Cloud
+        position={[4, -5, 10]}
+        scale={3}
+        opacity={0.3}
+        rotation={[0, 0.3 * Math.PI, 0]}
+      />
+      <Cloud
+        position={[3, 0, -10]}
+        scale={2}
+        opacity={0.3}
+        rotation={[0, -0.8 * Math.PI, 0]}
+      />
+      <Cloud
+        position={[10, 8, -15]}
+        scale={2.5}
+        opacity={0.3}
+        rotation={[0, -0.2 * Math.PI, 0]}
+      />
+      <Cloud
+        position={[8, 13, 8]}
+        scale={2}
+        opacity={0.3}
+        rotation={[0, 0.2 * Math.PI, 0]}
+      />
+
+      <hemisphereLight color={0x884488} groundColor={0x222255} intensity={3} />
+      <pointLight color={0xffdddd} position={[3, 1, -3]} intensity={50} />
+      <pointLight color={0xddddff} position={[-3, -1, 3]} intensity={50} />
     </>
   );
 }
