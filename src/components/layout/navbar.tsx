a76c386a91097f551959fef7fdb224a53d0b2fab
9rotama <aoim99.sc@gmail.com>
2024-04-29 01:29:55 +0900

    feat: hover effects of navbar links

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import clsx from "clsx";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/" className={styles.title}>
+    <nav className={styles.nav}>
+      <Link href="/" className={clsx(styles.title, styles.link)}>
         <span>9rtm</span>
         <span className={styles.tld}>.dev</span>
       </Link>
