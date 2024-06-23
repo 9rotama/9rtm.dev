8e839fd3634545c40cb9b354fe343ec2aae66783
9rotama <aoim99.sc@gmail.com>
2024-06-23 23:45:15 +0900

    feat: about ページ内容とリンク改善

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,29 +1,32 @@
-import Link from "next/link";
-import styles from "./page.module.css";
-import clsx from "clsx";
-import Image from "next/image";
+import SnsLink from "@/components/about/sns-link";
 import Article from "@/components/common/article";
-type SnsLink = {
+import GitHub from "@/assets/sns/github.svg";
+import Zenn from "@/assets/sns/zenn.svg";
+import ItchIo from "@/assets/sns/itch-io.svg";
+
+import styles from "./page.module.css";
+
+export type Sns = {
   name: string;
   href: string;
-  class: string;
+  iconSrc: string;
 };
 
-const snsLinks: SnsLink[] = [
+const snsLinks: Sns[] = [
   {
     name: "GitHub",
     href: "https://github.com/9rotama",
-    class: styles.github,
+    iconSrc: GitHub.src,
   },
   {
     name: "Zenn",
     href: "https://zenn.dev/9rotama",
-    class: styles.zenn,
+    iconSrc: Zenn.src,
   },
   {
     name: "itch.io",
     href: "https://9rotama.itch.io/",
-    class: styles.itchio,
+    iconSrc: ItchIo.src,
   },
 ];
 
@@ -31,80 +34,21 @@ export default function About() {
   return (
     <main>
       <Article>
-        <Image
-          loading="eager"
-          src="https://avatars.githubusercontent.com/u/65887771?v=4"
-          alt="avatar"
-          width={276}
-          height={276}
-          className={styles.avatar}
-        />
-        <ul className={styles.snslinks}>
-          {snsLinks.map((sns) => (
-            <li key={sns.name}>
-              <Link href={sns.href} aria-label={sns.name}>
-                <div className={clsx(styles.snslogo, sns.class)} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h2>about</h2>
+        <h1>about</h1>
+        <h2>🪪 me</h2>
         <p>
           Web フロントエンドを勉強しています。普段は Next.js, React, Typescript
           を使って開発しています。WebGL
           やレイトレなど、3DCG周りの技術に興味があります。
         </p>
-        <p>🎂: 2001/09/09</p>
-        <p>🗾: 福島 → 千葉</p>
-        <p>🎓: UoA</p>
-        <h2>like</h2>
-        <h3>フォント</h3>
-        <ul>
-          <li>欧文</li>
-          <ul>
-            <li>Helvetica Now</li>
-            <li>Avenir</li>
-            <li>Inter</li>
-          </ul>
-          <li>和文</li>
-          <ul>
-            <li>ニタラゴルイカ</li>
-            <li>ラグランパンチ</li>
-            <li>M PLUS</li>
-          </ul>
-        </ul>
-        <h3>ゲーム</h3>
-        <ul>
-          <li>やってる: Arcaea, jubeat, DJMAX, EZ2ON</li>
-          <li>始めた: SDVX, Dynamix</li>
-          <li>やってた: Cytus, BMS</li>
-        </ul>
-        <h2>used</h2>
-        <h3>メイン</h3>
-        <ul>
-          <li>TypeScript</li>
-          <li>JavaScript</li>
-          <li>Next.js</li>
-          <li>Three.js(r3f)</li>
-        </ul>
-        <h3>使ったことある</h3>
-        <ul>
-          <li>C#(Unity)</li>
-          <li>C++</li>
-          <li>Python</li>
-          <li>CUDA</li>
-          <li>OptiX</li>
-          <li>Gatsby.js</li>
-          <li>LINEBot SDK</li>
-          <li>Prisma</li>
-          <li>MongoDB</li>
-        </ul>
-        <h3>勉強中</h3>
-        <ul>
-          <li>Svelte</li>
-          <li>GLSL</li>
-          <li>Rust</li>
-        </ul>
+        <div className={styles.snsLinks}>
+          {snsLinks.map((sns) => (
+            <SnsLink key={sns.name} {...sns} />
+          ))}
+        </div>
+
+        <h2>💫 like</h2>
+        <p>リズムゲーム / お絵描き / フォント</p>
       </Article>
     </main>
   );
