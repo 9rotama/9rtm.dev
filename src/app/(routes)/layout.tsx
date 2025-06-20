ae8978c97fdffe036c492d6111998380cf8b7db7
9rotama <aoim99.sc@gmail.com>
2025-06-21 01:28:23 +0900

    chore: next/font & organize files

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -3,9 +3,14 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import WidthContainer from "@/components/width-container";
 import { siteUrl } from "@/constants/meta";
-import "@fontsource-variable/geist";
 import clsx from "clsx";
 import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist",
+});
 
 export const metadata: Metadata = {
   title: "9rtm.dev",
@@ -22,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={geist.variable}>
       <body className={clsx("overflow-x-hidden bg-bg text-fg-primary")}>
         <div className="flex flex-col items-center">
           <WidthContainer>
