2b0f602d0111f69eeb35bc449f67d62ffb4baf07
9rotama <aoim99.sc@gmail.com>
2025-07-10 03:10:51 +0900

    init: hello svelte

--- src/app/(routes)/_components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-24 flex w-full flex-col gap-6 px-12 text-center text-sm font-medium text-light/70 opacity-50">
-      &copy; {new Date().getFullYear()} 9rotama
-    </footer>
-  );
-}
