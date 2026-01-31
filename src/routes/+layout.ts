2c42d215640b68795431b93fc49046e3b6320084
9rotama <aoim99.sc@gmail.com>
2026-01-31 15:11:39 +0900

    fix: canonical url for each pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,11 @@
+import { PUBLIC_BASE_URL } from "$env/static/public";
+import type { LayoutLoad } from "./$types";
+
 export const prerender = true;
-export const trailingSlash = "always";
+export const trailingSlash = "never";
+
+export const load: LayoutLoad = ({ url }) => {
+  return {
+    canonicalUrl: new URL(url.pathname, PUBLIC_BASE_URL).toString(),
+  };
+};
