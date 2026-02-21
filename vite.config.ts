0e553e8c734495c3cd397427f32fc530b1cf3086
9rotama <aoim99.sc@gmail.com>
2026-02-21 17:57:05 +0900

    chore: update svelte pkgs
    chore: vite upgrade
    
    remove threlte studio import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { threlteStudio } from "@threlte/studio/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [threlteStudio(), tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit()],
   define: {
     __BUILD_TIME__: JSON.stringify(
       new Date().toLocaleDateString("ja-JP", {
