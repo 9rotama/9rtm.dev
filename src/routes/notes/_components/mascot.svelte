7c345bd83a3e6469d087bf5ab45f7f2a05bb45e9
9rotama <aoim99.sc@gmail.com>
2026-03-20 23:46:04 +0900

    feat: mascot svg dark

--- src/routes/notes/_components/mascot.svelte
+++ src/routes/notes/_components/mascot.svelte
@@ -1,35 +1,44 @@
-<svg viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg
+  viewBox="0 0 150 150"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="mascot"
+>
   <path
     d="M103.5 89C106 82 115.406 83.4119 119.5 80C122.5 77.5 122.5 73.5 122.5 73.5"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="2"
   />
   <path
     d="M107 92.5C109 86.5 120.658 87.1225 124.5 82C127.5 78 127.5 73 127.5 73"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="2"
   />
-  <path d="M119 72L125 60.5L131.5 72" stroke="#3D2C58" stroke-width="2" />
+  <path
+    d="M119 72L125 60.5L131.5 72"
+    stroke="var(--mascot-stroke)"
+    stroke-width="2"
+  />
   <path
     d="M82.5 104L80 86.5L90.5 82.65L95 81L107.5 93L100.5 107.5L82.5 104Z"
-    fill="#F5F1FF"
+    fill="var(--mascot-fill)"
   />
   <path
     d="M100.5 107.5L82.5 104L80 86.5L90.5 82.65M100.5 107.5L107.5 93L95 81L90.5 82.65M100.5 107.5L90.5 82.65"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
   />
   <path
     d="M38 44.087L69.3853 24L112 28.9677L109.483 69.4348L93.3094 90L47.0612 83.7826L38 44.087Z"
-    fill="#F5F1FF"
+    fill="var(--mascot-fill)"
   />
   <path
     d="M38 44.087L69.3853 24L112 28.9677M38 44.087L47.0612 83.7826L93.3094 90M38 44.087L92.3673 50.7826M93.3094 90L109.483 69.4348L112 28.9677M93.3094 90L92.3673 50.7826M112 28.9677L92.3673 50.7826"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
   <path
     d="M63.5 64L44.5 61C44.5 61 44.5 76.3947 55 77.5C65.5 78.6053 63.5 64 63.5 64Z"
-    fill="#F5F1FF"
+    fill="var(--mascot-fill)"
   />
   <mask
     id="mask0_156_5"
@@ -48,14 +57,14 @@
   <g mask="url(#mask0_156_5)">
     <path
       d="M52 79L51 66.5L59.5 67.5L60.5 80L52 79Z"
-      fill="#3D2C58"
-      stroke="#3D2C58"
+      fill="var(--mascot-stroke)"
+      stroke="var(--mascot-stroke)"
       stroke-width="0.7"
     />
   </g>
   <path
     d="M63.5 64L44.5 61C44.5 61 44.5 76.3947 55 77.5C65.5 78.6053 63.5 64 63.5 64Z"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
   <mask
@@ -75,100 +84,142 @@
   <g mask="url(#mask1_156_5)">
     <path
       d="M77 81L76.5 69L85 70L85.5 83.5L77 81Z"
-      fill="#3D2C58"
-      stroke="#3D2C58"
+      fill="var(--mascot-stroke)"
+      stroke="var(--mascot-stroke)"
       stroke-width="0.7"
     />
   </g>
   <path
     d="M90.9999 66.5L70.4999 64.5C70.4999 64.5 70.9998 80.06 81.9999 80.5C93 80.94 90.9999 66.5 90.9999 66.5Z"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
-  <path d="M51 36L64.3333 13L71 34.2667L66.8974 42L51 36Z" fill="#F5F1FF" />
+  <path
+    d="M51 36L64.3333 13L71 34.2667L66.8974 42L51 36Z"
+    fill="var(--mascot-fill)"
+  />
   <path
     d="M64.3333 13L51 36L66.8974 42M64.3333 13L71 34.2667L66.8974 42M64.3333 13L66.8974 42"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
   <path
     d="M77 43L85.6889 14L100 41.4324L77 43Z"
-    fill="#F5F1FF"
-    stroke="#3D2C58"
+    fill="var(--mascot-fill)"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
   <path
     d="M54.5 37L61.5 24.5L62.5 40"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
     stroke-linejoin="round"
   />
   <path
     d="M82.5 42.5C82.5 41.7 85.1667 30.8333 86.5 25.5L95 41.5"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
     stroke-linejoin="round"
   />
   <path
     d="M127 104.5L13.5003 84C13.5003 84 4.00053 137 59.0003 145.5C114 154 127 104.5 127 104.5Z"
-    fill="#F5F1FF"
+    fill="var(--mascot-fill)"
   />
-  <path d="M13 84L127.5 104.5" stroke="#3D2C58" />
+  <path d="M13 84L127.5 104.5" stroke="var(--mascot-stroke)" />
   <path
     d="M40.5 131.5L60.5 97.5L91 105L73 142.5L40.5 131.5Z"
-    fill="#F5F1FF"
-    stroke="#3D2C58"
+    fill="var(--mascot-fill)"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
-  <path d="M68.5 101.5L66 102.5L67 105" stroke="#3D2C58" stroke-width="0.7" />
-  <path d="M77.5 109L82.5 104.5" stroke="#3D2C58" stroke-width="0.7" />
+  <path
+    d="M68.5 101.5L66 102.5L67 105"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
+  <path
+    d="M77.5 109L82.5 104.5"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
   <path
     d="M82.5 109.5L86 108.5L84.5 105.5"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
   />
-  <path d="M69.5 103.5L76 105.5" stroke="#3D2C58" stroke-width="0.7" />
-  <path d="M70 136.5L47.5 128.5" stroke="#3D2C58" stroke-width="0.7" />
-  <path d="M74 130.5L61 125.5" stroke="#3D2C58" stroke-width="0.7" />
-  <path d="M57 124.5L50.5 122" stroke="#3D2C58" stroke-width="0.7" />
-  <path d="M77 119.5L78.5 122.5L74.5 124" stroke="#3D2C58" stroke-width="0.7" />
+  <path
+    d="M69.5 103.5L76 105.5"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
+  <path
+    d="M70 136.5L47.5 128.5"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
+  <path
+    d="M74 130.5L61 125.5"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
+  <path
+    d="M57 124.5L50.5 122"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
+  <path
+    d="M77 119.5L78.5 122.5L74.5 124"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
   <path
     d="M67 115.5L63.5 116.5L64.5 119.5"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
   />
-  <path d="M74 121L67 118" stroke="#3D2C58" stroke-width="0.7" />
-  <path d="M73.5 114.5L77 113L74.5 109.5" stroke="#3D2C58" stroke-width="0.7" />
+  <path d="M74 121L67 118" stroke="var(--mascot-stroke)" stroke-width="0.7" />
+  <path
+    d="M73.5 114.5L77 113L74.5 109.5"
+    stroke="var(--mascot-stroke)"
+    stroke-width="0.7"
+  />
   <path
     d="M72.5 111.5C68.9 110.3 66.6667 109.333 66 109"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
   />
-  <path d="M64 108.5L60 107" stroke="#3D2C58" stroke-width="0.7" />
+  <path d="M64 108.5L60 107" stroke="var(--mascot-stroke)" stroke-width="0.7" />
   <path
     d="M27.5 64C23.9999 66 25.5 69 25.5 69L50 106L58.5 111.5L55.5 101L33 65C33 65 31 62 27.5 64Z"
-    fill="#F5F1FF"
-    stroke="#3D2C58"
+    fill="var(--mascot-fill)"
+    stroke="var(--mascot-stroke)"
     stroke-linejoin="round"
   />
   <path
     d="M99 115.503C93.0001 115.95 88 111.679 88 106.003C87.9999 99.5034 92.8389 95.8724 98.5 96.0034C103.885 96.128 108.359 100.003 108.5 105.503C108.641 111.003 105 115.057 99 115.503Z"
-    fill="#F5F1FF"
-    stroke="#3D2C58"
+    fill="var(--mascot-fill)"
+    stroke="var(--mascot-stroke)"
   />
   <path
     d="M46.5 101.5C40.9354 102.37 35.4312 99.0547 34.5 93.5C33.5571 87.8756 37.8599 82.8441 43.5 82C49.1796 81.15 54.8346 84.7958 55.5 90.5C56.143 96.013 51.9837 100.642 46.5 101.5Z"
-    fill="#F5F1FF"
-    stroke="#3D2C58"
+    fill="var(--mascot-fill)"
+    stroke="var(--mascot-stroke)"
   />
   <path
     d="M25 60C25 60 20.5 59.5 18.5 62.5C16.4999 65.5 18 68.5 18 68.5"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
   />
   <path
     d="M24 57.5C24 57.5 18.8594 56.9284 16.0001 60.5C13.1407 64.0716 14.5 68 14.5 68"
-    stroke="#3D2C58"
+    stroke="var(--mascot-stroke)"
     stroke-width="0.7"
   />
 </svg>
+
+<style>
+  .mascot {
+    --mascot-fill: var(--color-background);
+    --mascot-stroke: var(--color-muted);
+  }
+</style>
