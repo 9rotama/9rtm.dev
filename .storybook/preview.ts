52222568f6f165a8494c02b5d6b73e8d46444a3d
9rotama <aoim99.sc@gmail.com>
2026-09-12 18:56:01 +0900

    fix: align Storybook font loading

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,7 @@
 import type { Preview, Renderer } from "@storybook/sveltekit";
 import { withThemeByClassName } from "@storybook/addon-themes";
+import "@fontsource-variable/geist-mono";
 import "@fontsource-variable/mona-sans";
-import "@fontsource/iosevka/400.css";
-import "@fontsource/iosevka/700.css";
-import "@fontsource/m-plus-1/400.css";
-import "@fontsource/m-plus-1/700.css";
 import "../src/app.css";
 
 const preview: Preview = {
