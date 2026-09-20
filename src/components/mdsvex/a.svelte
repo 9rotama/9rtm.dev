9bcf07f8713d29293cac5365e0f91e01bc98393a
9rotama <aoim99.sc@gmail.com>
2026-09-20 12:29:22 +0900

    style: refine inline link colors

--- src/components/mdsvex/a.svelte
+++ src/components/mdsvex/a.svelte
@@ -8,7 +8,7 @@
 
 <a
   {...rest}
-  class="text-foreground hover:text-foreground decoration-accent underline underline-offset-4 transition-colors duration-100 hover:decoration-3"
+  class="text-link decoration-link/30 hover:text-foreground underline underline-offset-4 transition-colors duration-100 hover:decoration-2"
 >
   {@render children()}
 </a>
